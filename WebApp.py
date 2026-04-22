676acc23e8724face75fb8145a72e5eec810dd12
Prince <princedev@pycentric.co.za>
2026-04-22 10:47:28 +0200

    Minor: added description of me with corresponding headers

---
+++ WebApp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Prince Phiri's Portfolio",page_icon="😁",layout="wide")
+
+st.subheader("Hello, My Name Is Prince Phiri 🖐🖐")
+st.title("A Software Engineer")
+st.subheader("About Me🤴🏾")
+st.write("I Am a Junior Developer trying to make a career in the tech world as best as I can by learning skills and applying them into real world solutions,This was Never Truly a passion of mine at first but now it become a wonderful and eternal learning journey that i an happy to be a part of.....most times😂")
+
